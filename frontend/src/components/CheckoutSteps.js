5d270baec01d43a7655d5f83dab0252a7d5e0dbc
Andrew <asgravett@live.co.za>
2021-01-15 17:36:37 +0200

    payment screen and save payment method

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -4,7 +4,7 @@ import {LinkContainer} from 'react-router-bootstrap';
 
 const CheckoutSteps = ({step1, step2, step3, step4}) => {
     return (
-        <Nav calssName='justify-content-center mb-4'>
+        <Nav className='justify-content-center mb-4'>
             
             <Nav.Item>
                 {step1 ? (
